223b7fee0380250709845f00d28e8e7038138256
Dolinin2021 <programmistdolinin@mail.ru>
2022-02-18 23:53:44 +0600

    Using method slugify()

--- phones/management/commands/import_phones.py
+++ phones/management/commands/import_phones.py
@@ -2,6 +2,7 @@ import csv
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+from django.utils.text import slugify
 
 
 class Command(BaseCommand):
@@ -17,4 +18,5 @@ class Command(BaseCommand):
             print(phone)
             Phone.objects.create(id=phone['id'], name=phone['name'],
                                  image=phone['image'], price=phone['price'],
-                                 release_date=phone['release_date'], lte_exists=phone['lte_exists'])
+                                 release_date=phone['release_date'], lte_exists=phone['lte_exists'],
+                                 slug = slugify(phone['name'], allow_unicode=True))
